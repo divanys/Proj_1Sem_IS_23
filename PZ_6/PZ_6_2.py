148e09ae4c34c60877088b6b6a52a7e79fa24119
divanys <hlinakcurbag@gmail.com>
2022-11-06 20:42:01 +0300

    добавление обработки исключений

--- PZ_6/PZ_6_2.py
+++ PZ_6/PZ_6_2.py
@@ -4,6 +4,18 @@
 from random import randint
 
 
+def inp():  # Функция обработки исключения
+    a = input('Введите размер списка: ')
+    while type(a) != int:
+        try:
+            a = int(a)
+            if a <= 0:
+                a = input('Введите целое число: ')
+        except ValueError:
+            a = input('Введите целое число: ')
+    return a
+
+
 def minObj(predel):  # Функция со сравнением следующего и предыдущего элемента относительно текущего в списке
     lst = []
     for _ in range(predel):
@@ -12,11 +24,9 @@ def minObj(predel):  # Функция со сравнением следующе
     for _ in range(len(lst)):
         if lst[_] < lst[_ + 1] and lst[_] < lst[_ - 1]:
             numba = _ + 1
-
             return lst[_], lst, numba
 
 
 # Вывод локального минимума и его идентификатора
-
-LocMin, AllList, numb = minObj(int(input("Введите предел списка   ")))
+LocMin, AllList, numb = minObj(inp())
 print(f"Первый локальный минимум списка {AllList}: {LocMin} под номером {numb}")
