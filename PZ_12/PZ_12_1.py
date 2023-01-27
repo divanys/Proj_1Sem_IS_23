78cbfef313773edd63d65ef714611f51084cd68a
divanys <hlinakcurbag@gmail.com>
2023-01-27 22:34:50 +0300

    pz12

--- PZ_12/PZ_12_1.py
+++ PZ_12/PZ_12_1.py
@@ -5,11 +5,12 @@
 
 from random import randint
 
-lstRandom = [randint(1, 100) for i in range(int(input("Введите количество чисел: ")))]  # создаём список с рандомными значениями
+lstRandom = [randint(1, 100) for i in
+             range(int(input("Введите количество чисел: ")))]  # создаём список с рандомными значениями
 lstFor3 = list(filter(lambda x: x % 3 == 0, lstRandom))  # список со значениями, кратными 3
 lstOther = list(filter(lambda x: x not in lstFor3, lstRandom))  # список с другими значениями
-print("Случайная последовательность чисел:", lstRandom, "\n", "Кратные трём:", lstFor3, "их количество:", len(lstFor3), "\n", "Не кратны трём:", lstOther, "их количество:", len(lstOther))
-
+print("Случайная последовательность чисел:", lstRandom, "\n", "Кратные трём:", lstFor3, "их количество:", len(lstFor3),
+      "\n", "Не кратны трём:", lstOther, "их количество:", len(lstOther))
 
 # решение в 2 строки:
 # lstRandom = [randint(1, 100) for i in range(int(input("Введите количество чисел: ")))]
